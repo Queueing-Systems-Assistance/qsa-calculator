bca2b6d1625d9e8980a20dccb18e194e69abf3dd
Szilágyi Zoltán <zoltan.szilagyi.cse@gmail.com>
2020-11-07 00:41:13 +0100

    QSA-54 Force tag if already exists

--- support/pipeline/pipeline.sh
+++ support/pipeline/pipeline.sh
@@ -23,22 +23,20 @@ echo "BRANCH_NAME=${BRANCH_NAME}"
 echo "GITHUB_ENDPOINT=${GITHUB_ENDPOINT}"
 echo "RELEASE_VERSION=${RELEASE_VERSION}"
 
-echo "RELEASE_VERSION=${RELEASE_VERSION}" >> gradle.properties
-
 # Build
-printf "Build QSA Calculator"
+echo "Build QSA Calculator"
 ./gradlew clean build -Prelease.version="${RELEASE_VERSION}" || exit
 
 # Create git tag & push to GitHub
-# git tag -a "${RELEASE_VERSION}" -m "${RELEASE_VERSION}" || exit
-# git push "${GITHUB_ENDPOINT}" refs/tags/"${RELEASE_VERSION}" || exit
+git tag -a -f "${RELEASE_VERSION}" -m "${RELEASE_VERSION}" || exit
+git push -f "${GITHUB_ENDPOINT}" refs/tags/"${RELEASE_VERSION}" || exit
 
-## Docker Login
-#echo "${DOCKER_PASSWORD}" | docker login docker.pkg.github.com -u "${DOCKER_USERNAME}" --password-stdin || exit
-#
-## Docker Build Image
-#sudo docker build \
-#-f ./support/docker/Dockerfile \
-#-t docker.pkg.github.com/queueing-systems-assistance/qsa-calculator/qsa-calculator:"${SELECTED_VERSION}" . || exit
-#
-#sudo docker push docker.pkg.github.com/queueing-systems-assistance/qsa-calculator/qsa-calculator:"${SELECTED_VERSION}" || exit
+# Docker Login
+echo "${DOCKER_PASSWORD}" | docker login docker.pkg.github.com -u "${DOCKER_USERNAME}" --password-stdin || exit
+
+# Docker Build Image
+sudo docker build \
+-f ./support/docker/Dockerfile \
+-t docker.pkg.github.com/queueing-systems-assistance/qsa-calculator/qsa-calculator:"${SELECTED_VERSION}" . || exit
+
+sudo docker push docker.pkg.github.com/queueing-systems-assistance/qsa-calculator/qsa-calculator:"${SELECTED_VERSION}" || exit
