443db3c473a7100c12fd0973e14dfcb732e721cc
Csanád Kölcsei <csanad.kolcsei@nebu.local>
2020-12-09 12:22:08 +0100

    QSA-101 modified i18n

--- qsa-calculator-implementation/src/main/java/com/unideb/qsa/calculator/implementation/validator/specific/LambdaTimesEsSmallerFrom1.java
+++ qsa-calculator-implementation/src/main/java/com/unideb/qsa/calculator/implementation/validator/specific/LambdaTimesEsSmallerFrom1.java
@@ -20,7 +20,9 @@ public class LambdaTimesEsSmallerFrom1 extends FeatureValidator {
     public Optional<ValidationErrorResponse> validate(Map<SystemFeature, Double> features, String featureId) {
         validatePresentFeatures(features, SystemFeature.eS, SystemFeature.Lambda);
         Optional<ValidationErrorResponse> result = Optional.empty();
-        if (features.get(SystemFeature.eS) >= features.get(SystemFeature.Lambda)) {
+        final double Lambda = features.get(SystemFeature.Lambda);
+        final double eS = features.get(SystemFeature.eS);
+        if (Lambda * eS >= 1) {
             result = Optional.of(new ValidationErrorResponse.Builder()
                     .withErrorMessage("error.smaller.parameter.eSFromLambda")
                     .withInputIds(List.of(SystemFeature.eS.name(), SystemFeature.Lambda.name()))
